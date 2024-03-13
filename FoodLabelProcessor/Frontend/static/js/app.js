8ed6d0222af8c40d032fabdd55ed3cdc1be55454
Byron Urrutia <b.urrutia@me.com>
2024-03-12 17:46:51 -0700

    added bootstrap and exported the js script

---
+++ FoodLabelProcessor/Frontend/static/js/app.js
@@ -0,0 +1,73 @@
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const captureButton = document.getElementById("capture");
+const retakeButton = document.getElementById("retake");
+const startButton = document.getElementById("start");
+const pauseButton = document.getElementById("pause");
+
+let stream = null;
+
+function manageStream(isStarting) {
+  if (isStarting) {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then(function (mediaStream) {
+        stream = mediaStream;
+        video.srcObject = stream;
+        video.play();
+        video.style.display = "block";
+        canvas.style.display = "none";
+        captureButton.style.display = "inline-block";
+        retakeButton.style.display = "none";
+        startButton.style.display = "none";
+        pauseButton.style.display = "inline-block";
+      })
+      .catch(function (err) {
+        console.log("An error occurred: " + err);
+      });
+  } else {
+    if (stream) {
+      stream.getTracks().forEach((track) => track.stop());
+    }
+    video.style.display = "none";
+    captureButton.style.display = "none";
+    startButton.style.display = "inline-block";
+    pauseButton.style.display = "none";
+    retakeButton.style.display = "none"; // Ensure retake button is also managed correctly
+  }
+}
+
+// Capture the photo and show it
+captureButton.addEventListener("click", function () {
+  context.drawImage(video, 0, 0, 640, 480);
+  video.style.display = "none";
+  canvas.style.display = "block";
+  captureButton.style.display = "none";
+  retakeButton.style.display = "inline-block";
+  startButton.style.display = "none";
+  pauseButton.style.display = "none";
+});
+
+// Start the video stream
+startButton.addEventListener("click", function () {
+  manageStream(true);
+});
+
+// Pause the video stream
+pauseButton.addEventListener("click", function () {
+  manageStream(false);
+});
+
+// Retake the photo, showing video stream again
+retakeButton.addEventListener("click", function () {
+  manageStream(true);
+});
+
+// Initialize the page
+window.onload = function () {
+  startButton.style.display = "inline-block";
+  captureButton.style.display = "none";
+  retakeButton.style.display = "none";
+  pauseButton.style.display = "none";
+};
